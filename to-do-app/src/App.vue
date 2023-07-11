cde947b45974ef9a0cdcd71a52e5f8e612176ef5
Mariana Naselli <nasellimariana@gmail.com>
2023-07-12 01:54:59 +0200

    Agrego tareas

--- to-do-app/src/App.vue
+++ to-do-app/src/App.vue
@@ -1,60 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-container>
+      <v-form @submit.prevent="addTask">
+        <v-text-field
+          v-model="newTask"
+          label="Nueva tarea"
+          outlined
+          required
+        ></v-text-field>
+        <v-btn type="submit" color="primary">Agregar</v-btn>
+      </v-form>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+      <v-list>
+        <v-list-item
+          v-for="(task, index) in tasks"
+          :key="index"
+        >
+          <v-list-item-content>
+            {{ task }}
+          </v-list-item-content>
+          <v-list-item-action>
+            <!-- Agrega botones para cambiar el nombre, eliminar o confirmar la tarea -->
+            <!-- Por ejemplo, puedes usar <v-btn> con iconos de Vuetify -->
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tasks: [],
+      newTask: '', // Agrega la propiedad newTask en el objeto data
+    };
   },
-
-  data: () => ({
-    //
-  }),
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.tasks.push(this.newTask);
+        this.newTask = ''; // Limpia el campo de entrada después de agregar la tarea
+      }
+    },
+  },
 };
 </script>
+
