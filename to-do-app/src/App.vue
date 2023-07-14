75442f779c33333d1ddf66edd4a7ab28815298fe
Mariana Naselli <nasellimariana@gmail.com>
2023-07-14 04:52:43 +0200

    Agrego menu/nombre/iconos

--- to-do-app/src/App.vue
+++ to-do-app/src/App.vue
@@ -1,34 +1,70 @@
 <template>
-  <v-app>
-    <v-container>
-      <task-form @add="addTask"></task-form>
-      <task-list :tasks="tasks"></task-list>
-      
-    </v-container>
-    <router-view></router-view>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item router to="/">
+          <v-list-item-content>
+            <v-list-item-title>Task List</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item router to="/task/completed">
+          <v-list-item-content>
+            <v-list-item-title>Task Completed</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Task List</v-toolbar-title>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" text class="ml-auto">
+            {{ username }}
+            <v-icon right>mdi-menu-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click="logout">
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <task-form @add="addTask"></task-form>
+        <task-list :tasks="tasks"></task-list>
+      </v-container>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import TaskList from './components/TaskList.vue';
-import TaskForm from './components/TaskForm.vue';
-import api from './api.js';
+import TaskList from "./components/TaskList.vue";
+import TaskForm from "./components/TaskForm.vue";
+import api from "./api.js";
 
 export default {
   components: {
     TaskList,
-    TaskForm
+    TaskForm,
   },
   data() {
     return {
-      tasks: []
+      drawer: false,
+      tasks: [],
+      username: "John Doe", // Nombre de usuario (puedes obtenerlo de la autenticación)
     };
   },
   async mounted() {
     try {
       await this.fetchTasks();
     } catch (error) {
-      console.log('Error al obtener las tareas:', error);
+      console.log("Error al obtener las tareas:", error);
     }
   },
   methods: {
@@ -36,7 +72,7 @@ export default {
       try {
         this.tasks = await api.fetchTasks();
       } catch (error) {
-        console.log('Error al obtener las tareas:', error);
+        console.log("Error al obtener las tareas:", error);
         throw error;
       }
     },
@@ -44,21 +80,13 @@ export default {
       try {
         this.tasks.push(newTask);
       } catch (error) {
-        console.log('Error al agregar la tarea:', error);
+        console.log("Error al agregar la tarea:", error);
         throw error;
       }
-    }
-  }
+    },
+    logout() {
+      // Lógica para cerrar sesión
+    },
+  },
 };
 </script>
-
-
-
-
-
-
-
-
-
-
-
--- to-do-app/src/components/TaskList.vue
+++ to-do-app/src/components/TaskList.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        <span>{{ task.name }}</span>
-        <span>{{ task.person }}</span>
-        <span>{{ task.status }}</span>
-      </li>
-    </ul>
+    <v-list>
+      <v-list-item v-for="task in tasks" :key="task.id">
+        <v-list-item-content>
+          <v-list-item-title>{{ task.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ task.person }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-icon @click="editTask(task)">mdi-pencil</v-icon>
+          <v-icon @click="deleteTask(task.id)">mdi-delete</v-icon>
+          <v-icon @click="archiveTask(task.id)">mdi-archive</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
